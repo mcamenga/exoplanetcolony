34cd047454cdfd7c6bded2f80e30d94d9932da82
Mike Camenga <mike.camenga@gmail.com>
2015-08-08 15:55:03 -0400

    added button handling

--- js/game/states/MainMenu.js
+++ js/game/states/MainMenu.js
@@ -1,25 +1,36 @@
 ExoplanetColony.MainMenu = function() {};
 
 ExoplanetColony.MainMenu.prototype = {
+    
   create: function() {
     
     this.startB = this.add.sprite(this.game.world.centerX, this.game.world.centerY, 'startButton');
     this.startB.anchor.setTo(0.5);
+    this.startB.inputEnabled = true;
+    this.startB.events.onInputDown.add(this.startButtonClicked, this);
       
     this.aboutB = this.add.sprite(this.game.world.centerX, this.game.world.centerY + 128, 'aboutButton');
     this.aboutB.anchor.setTo(0.5);
+    this.aboutB.inputEnabled = true;
+    this.aboutB.events.onInputDown.add(this.aboutButtonClicked, this);
       
     this.howB = this.add.sprite(this.game.world.centerX, this.game.world.centerY + 256, 'howtoplayButton');
     this.howB.anchor.setTo(0.5);
-
-
-    //this.gameMusic = this.game.add.audio('gameMusic');
-    //this.gameMusic.play('', 0, true);
-
+    this.howB.inputEnabled = true;
+    this.howB.events.onInputDown.add(this.howButtonClicked, this);
   },
-  update: function() {
-    if(this.game.input.activePointer.justPressed()) {
-      this.game.state.start('Game');
+  update: function() {},
+    
+ startButtonClicked: function() {
+    //gamestart
+    console.log('start');
+    },
+aboutButtonClicked: function() {
+    //type in about text
+    console.log('about');
+    },
+ howButtonClicked: function() {
+    //gamestart
+    console.log('how');
     }
-  }
 };
